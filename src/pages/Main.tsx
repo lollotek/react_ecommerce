029d5b47acfad74c267c2d9a6831debe22e49ac3
Lorenzo <lorenzo.boasso@tui.com>
2024-01-13 11:52:05 +0100

    feat: header ui

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,27 +1,12 @@
-import { Box, Flex, Separator } from '@radix-ui/themes';
-import { Link, Outlet } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
+import Header from '@sections/Header';
 
 function Main() {
   return (
-    <Flex direction="column">
-      <Flex justify="between" height="9">
-        <Link to={'/'}>
-          <Box>
-              <h1>dummy-commerce</h1>
-          </Box>
-        </Link>
-        <Box height="9">
-          <Flex gap="3" align="center">
-            <Link to={'/'}>products</Link>
-          <Separator orientation="vertical" />
-            <Link to={'/cart'}>cart</Link>
-          </Flex>
-        </Box>
-      </Flex>
-      <Box>
-        <Outlet />
-      </Box>
-    </Flex>
+    <div>
+      <Header />
+      <Outlet />
+    </div>
   )
 }
 
