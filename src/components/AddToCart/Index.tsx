c791303e42dc5cc98d1efc0157134233dc821bd9
Lorenzo <lorenzo.boasso@tui.com>
2024-01-15 20:11:08 +0100

    fix: toast behaviour

--- src/components/AddToCart/Index.tsx
+++ src/components/AddToCart/Index.tsx
@@ -8,12 +8,12 @@ export type Props = {
 }
 
 export const AddToCartButton = ({ id, description } : Props) => {
-  const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(0);
   const { removeCartProduct, addCartProduct } = useCartProducts()
 
   const onSave = () => {
     addCartProduct(id)
-    setOpen(true)
+    setOpen(open +1)
   }
 
   const onUndo = () => {
@@ -22,30 +22,28 @@ export const AddToCartButton = ({ id, description } : Props) => {
 
   return (
     <div>
-      <Toast.Provider swipeDirection="right">
         <Button size={{ initial: '4', sm: '3' }} onClick={onSave}>
           Add to cart
         </Button>
-        <Toast.Root
-          className="bg-white rounded-md shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] p-[15px] grid [grid-template-areas:_'title_action'_'description_action'] grid-cols-[auto_max-content] gap-x-[15px] items-center data-[state=open]:animate-slideIn data-[state=closed]:animate-hide data-[swipe=move]:translate-x-[var(--radix-toast-swipe-move-x)] data-[swipe=cancel]:translate-x-0 data-[swipe=cancel]:transition-[transform_200ms_ease-out] data-[swipe=end]:animate-swipeOut"
-          open={open}
-          onOpenChange={setOpen}
-          duration={1600}
-        >
-          <Toast.Title className="[grid-area:_title] mb-[5px] font-medium text-slate12 text-[15px]">
-            <Text>Added to cart</Text>
-          </Toast.Title>
-          <Toast.Description asChild>
-            <Text>{description}</Text>
-          </Toast.Description>
-          <Toast.Action className="[grid-area:_action]" asChild altText="undo add to cart">
-            <Button size="2" variant="outline" onClick={onUndo}>
-              Undo
-            </Button>
-          </Toast.Action>
-        </Toast.Root>
-        <Toast.Viewport className="[--viewport-padding:_25px] fixed bottom-0 right-0 flex flex-col p-[var(--viewport-padding)] gap-[10px] w-[390px] max-w-[100vw] m-0 list-none z-[2147483647] outline-none" />
-      </Toast.Provider>
+          {Array.from({ length: open }).map((_, index) => (
+          <Toast.Root
+            className="bg-white rounded-md shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] p-[15px] grid [grid-template-areas:_'title_action'_'description_action'] grid-cols-[auto_max-content] gap-x-[15px] items-center data-[state=open]:animate-slideIn data-[state=closed]:animate-hide data-[swipe=move]:translate-x-[var(--radix-toast-swipe-move-x)] data-[swipe=cancel]:translate-x-0 data-[swipe=cancel]:transition-[transform_200ms_ease-out] data-[swipe=end]:animate-swipeOut"
+            key = {`id-${index}`}
+            duration={1600}
+          >
+            <Toast.Title className="[grid-area:_title] mb-[5px] font-medium text-slate12 text-[15px]">
+              <Text>Added to cart</Text>
+            </Toast.Title>
+            <Toast.Description asChild>
+              <Text>{description}</Text>
+            </Toast.Description>
+            <Toast.Action className="[grid-area:_action]" asChild altText="undo add to cart">
+              <Button size="2" variant="outline" onClick={onUndo}>
+                Undo
+              </Button>
+            </Toast.Action>
+          </Toast.Root>
+          ))}
     </div>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-import '@radix-ui/themes/styles.css';
 import { Theme } from '@radix-ui/themes';
 
 import { ApiProvider } from '@reduxjs/toolkit/query/react'
@@ -9,12 +8,18 @@ import { productsApi } from './api/products/index.tsx';
 
 import { RouterProvider } from "react-router-dom";
 import { router } from './services/router/router.tsx';
+import * as Toast from '@radix-ui/react-toast';
+
+import '@radix-ui/themes/styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ApiProvider api={productsApi}>
       <Theme>
-        <RouterProvider router={router} />        
+        <Toast.Provider swipeDirection="right">
+          <RouterProvider router={router} />        
+        <Toast.Viewport className="[--viewport-padding:_25px] fixed bottom-0 right-0 flex flex-col p-[var(--viewport-padding)] gap-[10px] w-[390px] max-w-[100vw] m-0 list-none z-[2147483647] outline-none" />
+        </Toast.Provider>
       </Theme>
     </ApiProvider>
   </React.StrictMode>,
