13d4e7ae76c003377c7bff8a0fc74bdc593156ae
Lorenzo <lorenzo.boasso@tui.com>
2024-01-15 18:23:29 +0100

    refactor: restyle navigation as amazon like

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -6,7 +6,6 @@ function Products() {
   return (
     <Section mt="0" size="1" mx={{ initial: '2', xs:'6'}}>
       <Text as="p" className="mt-4 text-xl font-bold tracking-tight text-gray-600 sm:text-2xl">All products</Text>
-      <Pagination />
       <ListItems />
       <Pagination />
     </Section>
